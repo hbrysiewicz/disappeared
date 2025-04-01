bb8c1c6c5fa0e9d70ca0366fd5ac6ce9a5313aeb
Alex Elliott <alex@starshot.tech>
2025-04-01 08:45:23 -0700

    Updates to config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
-  return defineConfig({
-    plugins: [react()],
-    define: {
-      "process.env.NODE_ENV": `"${mode}"`,
-    }
-  })
-}
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
