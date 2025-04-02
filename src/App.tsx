b63c7d625dd6c045c303d950745b931cdc447ca9
Alex Elliott <alex@starshot.tech>
2025-04-02 06:10:39 -0700

    Add more and update favicon

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,18 @@ function App() {
       <div className="pt-6 flex gap-4">
         <Card 
           name="Arturo Suárez Trejo" 
-          description="Suárez, a 33-year-old native of Caracas, Venezuela, made his last call to his wife on March 14, 2025. Later, she would notice him in the propoganda images released by his hummingbird tattoo on his neck." 
-          image="/20250325_Arturo-Suarez-Performing-and-on-call_990.webp"
+          description="Suárez, a 33-year-old native of Caracas, Venezuela, made his last call to his wife on March 14, 2025. Later, she would notice him in the propaganda images released by his hummingbird tattoo on his neck." 
+          image="/Arturo-Suarez.png"
         />
         <Card
           name="Neri Alvarado Borges"
-          description="Alvarado was deeply devoted to helping his autistic brother of 15. He was close with his boss, owner of two Venezuelen bakeries, who visited him after he was detained in early February."
-          image="/0326_tattoos-neri_400.webp"
+          description="Alvarado was deeply devoted to helping his autistic brother of 15. He was close with his boss, owner of two Venezuelan bakeries, who visited him after he was detained in early February."
+          image="/neri.png"
+        />
+        <Card
+          name="Rumeysa Ozturk"
+          description="The 30 year old PhD student was approached by 6 plain-clothes ICE agents on her way to dinner and kidnapped because of an op-ed piece she wrote in the school newspaper about the genocide in Palestinian by Israel"
+          image="/rumeysa.png"
         />
     </div>
     </>
